c6de433342d9e3bd1194f98db5d99f3a965b9f6e
paresiqbal <thecrowdel@gmail.com>
2023-09-16 13:43:29 +0700

    Adding photo scema

---
+++ models/Images.ts
@@ -0,0 +1,29 @@
+import {z} from "zod"
+
+const basicImageSchema = z.object({
+    page: z.number(),
+    per_page: z.number(),
+    prev_page: z.string().optional(),
+    next_page: z.string().option(),
+    total_result: z.number()
+})
+
+const photoSchema = z.object({
+    id: z.number(),
+    width: z.number(),
+    height: z.number(),
+    url: z.string(),
+    src: z.object({
+        large: z.string(),
+    }),
+    alt: z.string(),
+    blurredDataUrl: string().optional()
+})
+
+export const ImagesSchemaWithPhoto = basicImageSchema.extend({
+    photos: z.array(photoSchema)
+})
+
+export type Photo = z.infer<typeof photoSchema>
+
+export type ImageResults = z.infer<typeof ImagesSchemaWithPhoto>
