7501619db73db63e8ca05606c182a00afa45229d
paresiqbal <thecrowdel@gmail.com>
2023-09-22 22:34:31 +0700

    bind to gallery

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -2,6 +2,9 @@ import fetchImages from "@/lib/fetchImages";
 import type { ImagesResults } from "@/models/Images";
 import ImgContainer from "./ImgContainer";
 
+// helpers
+import addBluredDataUrl from "@/lib/getBase64";
+
 export default async function Gallery() {
   const url = "https://api.pexels.com/v1/curated";
 
@@ -10,9 +13,12 @@ export default async function Gallery() {
   // return response
   if (!images)
     return <h2 className="m-4 text-2xl font-bold">No Images Found</h2>;
+
+  const photoWithBlur = await addBluredDataUrl(images);
+
   return (
     <section className="px-2 my-3 grid gap-2 grid-cols-gallery ">
-      {images.photos.map((photo) => (
+      {photoWithBlur.map((photo) => (
         <ImgContainer photo={photo} key={photo.id} />
       ))}
     </section>
