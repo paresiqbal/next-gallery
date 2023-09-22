06b9d98b5979cbf80b37f1333633fb28b8fc6dcc
paresiqbal <thecrowdel@gmail.com>
2023-09-22 14:15:12 +0700

    asdas

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -1,5 +1,6 @@
 import fetchImages from "@/lib/fetchImages";
 import type { ImagesResults } from "@/models/Images";
+import ImgContainer from "./ImgContainer";
 
 export default async function Gallery() {
   const url = "https://api.pexels.com/v1/curated";
@@ -12,7 +13,7 @@ export default async function Gallery() {
   return (
     <section className="px-2 my-3 grid gap-2 grid-cols-gallery">
       {images.photos.map((photo) => (
-        <div className="h-64 bg-gray-200 rounded-xl" key={photo.id}></div>
+        <ImgContainer photo={photo} />
       ))}
     </section>
   );
