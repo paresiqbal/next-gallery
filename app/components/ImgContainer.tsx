7daf09e75576292879b2fdd3ef401994ce8fef33
paresiqbal <thecrowdel@gmail.com>
2023-09-22 22:13:53 +0700

    Add size

--- app/components/ImgContainer.tsx
+++ app/components/ImgContainer.tsx
@@ -8,15 +8,15 @@ type Props = {
 export default function ImgContainer({ photo }: Props) {
   return (
     <div
-      className="h-64 bg-gray-200 rounded-xl relative overflow-hidden"
+      className="h-64 bg-gray-200 rounded-xl relative overflow-hidden group"
       key={photo.id}
     >
       <Image
         src={photo.src.large}
         alt={photo.alt}
         fill={true}
-        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-        className="object-cover"
+        sizes="(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)"
+        className="object-cover group-hover:opacity-75"
       />
     </div>
   );
