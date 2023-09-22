fcacf80a85c636a6a96cbcac4f346bed4d45b145
paresiqbal <thecrowdel@gmail.com>
2023-09-22 22:36:11 +0700

    bind to image container

--- app/components/ImgContainer.tsx
+++ app/components/ImgContainer.tsx
@@ -16,6 +16,8 @@ export default function ImgContainer({ photo }: Props) {
         alt={photo.alt}
         fill={true}
         sizes="(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)"
+        placeholder="blur"
+        blurDataURL={photo.blurredDataUrl}
         className="object-cover group-hover:opacity-75"
       />
     </div>
