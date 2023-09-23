97276cbdf8445c2ff566896e166355a36eb48ffc
paresiqbal <thecrowdel@gmail.com>
2023-09-23 22:08:38 +0700

    Add heig

--- app/components/ImgContainer.tsx
+++ app/components/ImgContainer.tsx
@@ -28,6 +28,7 @@ export default function ImgContainer({ photo }: Props) {
             src={photo.src.large}
             alt={photo.alt}
             width={photo.width}
+            height={photo.height}
             sizes="(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)"
             placeholder="blur"
             blurDataURL={photo.blurredDataUrl}
