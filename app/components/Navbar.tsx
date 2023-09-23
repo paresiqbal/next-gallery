5d2b88be30ec3b77d2ef1119b588985f768c3e90
paresiqbal <thecrowdel@gmail.com>
2023-09-23 14:32:22 +0700

    Register search

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Search from "./Search";
 
 export default function Navbar() {
   return (
@@ -7,6 +8,7 @@ export default function Navbar() {
         <h1 className="text-2xl sm:text-3xl text-center whitespace-nowrap">
           <Link href={"/"}>Next JS Gallery</Link>
         </h1>
+        <Search />
       </nav>
     </header>
   );
