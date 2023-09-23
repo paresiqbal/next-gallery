f9fdf285cec9ac87611bfcdafa5ac2b0458ec258
paresiqbal <thecrowdel@gmail.com>
2023-09-23 14:24:21 +0700

    Adding style

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <header className="bg-black sticky top-0 z-10">
-      <nav>
+      <nav className="flex flex-col gap-4 sm:flex-row sm:justify-between items-center p-4 font-bold max-w-6xl mx-auto text-white">
         <h1>
           <Link href={"/"}>Gallery</Link>
         </h1>
