82e2d0adc81df06022232131b45167dfb89915db
paresiqbal <thecrowdel@gmail.com>
2023-09-23 14:29:17 +0700

    Add fungtionality

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,1 +1,16 @@
 "use client";
+
+import { useState, FormEvent } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Search() {
+  const [search, setSearch] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    router.push(`/result/${search}`);
+    setSearch("");
+  };
+  return <div>Search</div>;
+}
