52ed8fe41944980546c8149c731ade88f864c35b
paresiqbal <thecrowdel@gmail.com>
2023-09-24 20:05:25 +0700

    asdasd

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,12 +1,20 @@
 import Link from "next/link";
 
 type Props = {
-  topic: string,
-  page: string | undefined,
-  prevPage: string | null,
-  nextPage: string | null,
+  topic: string;
+  page: string | undefined;
+  prevPage: string | null;
+  nextPage: string | null;
 };
 
 export default function Footer({ topic, page, prevPage, nextPage }: Props) {
+  if (!prevPage && !nextPage) return;
+
+  const pageNums: number[] = [];
+  if (prevPage && nextPage) {
+    for (let i = parseInt(prevPage) + 1; i < parseInt(nextPage); i++) {
+      pageNums.push(i);
+    }
+  }
   return <div>Footer</div>;
 }
